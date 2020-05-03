2ec0dfbabadac4c82d10b1d304172a04a5d97696
tulsa <tulsa@Optimus>
2020-05-04 01:38:04 +0530

    Testing Sample Project Commit

--- ConnectToDB/src/connectPkg/ConnectToDB.java
+++ ConnectToDB/src/connectPkg/ConnectToDB.java
@@ -16,27 +16,31 @@ import java.sql.Statement;
 public class ConnectToDB {
 
 	/**
-	 * @param args
+	 * 
+	 * @param query
+	 * @param props which should include database name, username and password separated by | respectively
+	 * @return 
 	 */
-	public static void main(String[] args) {
-		String url="jdbc:mysql://localhost:3306/ashish";
-		String user="root";
-		String password="root";
-		Connection connect;
+	public ResultSet initConnection(String query, String props) {
+
+		String[] dbProp=props.split("\\|");
+		String url="jdbc:mysql://localhost:3306/"+dbProp[0];
+		Connection connect=null;
+		ResultSet rs=null;
 		try {
-			connect = DriverManager.getConnection(url, user, password);
+			connect = DriverManager.getConnection(url, dbProp[1], dbProp[2]);
 			Statement stmt= connect.createStatement();
-			ResultSet rs=stmt.executeQuery("SELECT * FROM student");
+			rs= stmt.executeQuery(query);
 			while(rs.next()) {
-				System.out.println("Roll no :"+rs.getInt("studId")+" Name: "+rs.getString("studNm"));
+				System.out.println("Roll no: "+rs.getInt("studId")+" Name:"+rs.getString("studNm"));
 			}
 			connect.close();
 		} catch (SQLException e) {
 			System.out.println("Connection failed");
 			e.printStackTrace();
 		}
-		
-
+		return rs;
 	}
+
 
 }
